94ec0bb41ecb79223551ea25f6aa363414821b80
FAIRDEVX <115607341+FAIRDEVX@users.noreply.github.com>
2023-09-11 00:20:42 +0700

    update

--- src/PostForm.jsx
+++ src/PostForm.jsx
@@ -1,18 +1,40 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-export default class PostForm extends Component {
-  render() {
-    return (
-      <div>
-            <h1>Create Post</h1>
-            <form action="">
-                <input type="text" placeholder='Enter post title' required />
-                <br /><br />
-                <textarea cols="30" rows="5" placeholder='Enter post' required />
-                <br /><br />
-                <button>Post</button>
-            </form>
-      </div>
-    )
-  }
+class PostForm extends Component {
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const title = this.getTitle.value;
+        const message = this.getMessage.value;
+        const data = {
+            id: new Date(),
+            title,
+            message,
+            editing: false
+        }
+        this.props.dispatch({
+            type: 'ADD_POST',
+            data
+        })
+        this.getTitle.value = "";
+        this.getMessage.value = "";
+    }
+
+    render() {
+        return (
+            <div className="post-container">
+                <h1 className="post_heading">Create Post</h1>
+                <form className="form" onSubmit={this.handleSubmit}>
+                    <input type="text" ref={(input) => this.getTitle = input} placeholder="Enter post title" required />
+                    <br/><br/>
+                    <textarea cols="30" ref={(input) => this.getMessage = input} rows="5" placeholder="Enter post" required />
+                    <br/><br/>
+                    <button>Post</button>
+                </form>
+            </div>
+        )
+    }
 }
+
+export default connect()(PostForm)
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import postReducer from "./reducers/postReducers.jsx";
+const store = createStore(postReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
