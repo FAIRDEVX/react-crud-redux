c7d5a6f0984accd676d435e0743923af58fa5c1e
FAIRDEVX <115607341+FAIRDEVX@users.noreply.github.com>
2023-09-10 23:03:24 +0700

    update
    creaate repo react & redux

---
+++ src/PostForm.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class PostForm extends Component {
+  render() {
+    return (
+      <div>
+            <h1>Create Post</h1>
+            <form action="">
+                <input type="text" placeholder='Enter post title' required />
+                <br /><br />
+                <textarea cols="30" rows="5" placeholder='Enter post' required />
+                <br /><br />
+                <button>Post</button>
+            </form>
+      </div>
+    )
+  }
+}
